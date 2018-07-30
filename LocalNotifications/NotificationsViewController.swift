4d40bea124c7d04d4fc2691b0d72257e6d26012e
mfouda214 <m.fouda@robone.net>
2018-07-30 21:52:18 +0100

    import Usernotifications

---
+++ LocalNotifications/NotificationsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  LocalNotifications
+//
+//  Updated by Mohamed Sobhi Fouda on 7/30/18.
+//  Created by Hesham Abd-Elmegid on 3/7/17.
+//  Copyright © 2017 CareerFoundry. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class NotificationsViewController: UIViewController {
+    
+    let notificationCenter = UNUserNotificationCenter.current()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+    @IBAction func requestNotificationsAuthorization(_ sender: Any) {
+        
+    }
+    
+    @IBAction func checkNotificationsAuthorizationStatus(_ sender: Any) {
+
+    }
+    
+    @IBAction func showNotification(_ sender: Any) {
+
+    }
+}
